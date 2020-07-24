6d3730db759ceda2046ff7efb8c232fde8bb91bc
czyzewski03 <czyzewski03@gmail.com>
2020-07-24 19:32:32 +0100

    Feed can be displayed in terminal using an argument

--- src/main.c
+++ src/main.c
@@ -2,21 +2,22 @@
 #include "update.h"
 
 int main(int argc, char **argv) {
-	//GtkApplication *application;
-	//int status;
 
-	//application = gtk_application_new("org.mzyzc.forage", G_APPLICATION_FLAGS_NONE);
-	//g_signal_connect(application, "activate", G_CALLBACK(activate), NULL);
-	//status = g_application_run(G_APPLICATION(application), argc, argv);
-	//g_object_unref(application);
-	
-	//return status;
-	
 	if (argc != 2) {
-		printf("Please provide an XML file to parse\n");
-		return 1;
+		g_print("Please provide an XML file to parse\n");
+	}
+	else {
+		update_feed(argv[1]);
 	}
 
-	update_feed(argv[1]);
+	GtkApplication *application;
+	int status;
+
+	application = gtk_application_new("org.mzyzc.forage", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(application, "activate", G_CALLBACK(activate), NULL);
+	status = g_application_run(G_APPLICATION(application), argc, argv);
+	g_object_unref(application);
+	
+	return status;
 }
 
